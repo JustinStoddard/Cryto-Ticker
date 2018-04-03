9d19f2f9735a068dac6d8195999b6243feeac7fb
Justin Stoddard <Stoddard.j.t@gmail.com>
2018-04-03 10:58:12 -0600

    new Files

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -4,4 +4,22 @@ class Coin < ApplicationRecord
 
   has_many :watched_coins, through: :destroy
   has_many :users, through: :watched_coins
+
+  def self.create_by_cmc_id(res)
+    if /^2\d\d$/ =~ res.code.to_s 
+      match = res[0].with_inifferent_access
+      coin_params = { 
+        name: match[:name],
+        symbol: match[:symbol],
+        cmc_id: match[:id]
+      }
+
+      Coin.find_or_create_by(coin_params) do |coin|
+        coin.price = match[:price_usd]
+        coin.last_fetched = DateTime.now
+    else
+      nil
+    end
+  end 
+  
 end
